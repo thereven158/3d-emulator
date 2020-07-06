b9e1045a3440e364d7162b5da960e70f44414c33
Muhammad Yoga Nugraha Putra <myoga82@rocketmail.com>
2020-07-06 18:23:31 +0700

    add button instantiate

--- Assets/Scripts/InstantiateButton.cs
+++ Assets/Scripts/InstantiateButton.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public class InstantiateButton : MonoBehaviour
 {
     private string _urlDirectory;
+	private string _nameVideo = "cat";
 
-    public int LoadVideos(string directory)
+	[SerializeField]
+	private GameObject _btnPrefab;
+
+	[SerializeField]
+	private GameObject _videoBtnsObj;
+
+	[SerializeField]
+	private VideoLoader _videoLoader;
+	
+	[SerializeField]
+	private UIControl _uiControl;
+
+    public int CountVideos(string directory)
     {
         int i = 0;
 
@@ -17,8 +31,12 @@ public class InstantiateButton : MonoBehaviour
 
         foreach (FileInfo fileInfo in _fileInfo)
         {
-            if (fileInfo.Extension.Contains("mp4"))
-                i++;
+            if (fileInfo.Extension.Contains("mp4")){
+				i++;
+				int temp = i;
+				CreateButton(temp);
+			}
+                
         }
 
         return i;
@@ -29,7 +47,7 @@ public class InstantiateButton : MonoBehaviour
     {
         _urlDirectory = Application.streamingAssetsPath + "/" + "Videos/";
 
-        Debug.Log(LoadVideos(_urlDirectory));
+        CountVideos(_urlDirectory);
     }
 
     // Update is called once per frame
@@ -37,4 +55,20 @@ public class InstantiateButton : MonoBehaviour
     {
         
     }
+
+	private void CreateButton(int number){
+		GameObject newButton = Instantiate(_btnPrefab) as GameObject;
+		newButton.transform.SetParent(_videoBtnsObj.transform, false);
+
+		Button btnComponent = newButton.GetComponent<Button>();
+
+		string nameFile = _nameVideo + number;
+
+		btnComponent.onClick.AddListener(() => {
+			_videoLoader.PlayStreamingVideo(nameFile);
+			_uiControl.EnableVideoControlUI();
+			_uiControl.DisableMainUI();
+		});
+	}
+
 }
